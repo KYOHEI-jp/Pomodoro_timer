7c45fe4323d543e72820b47b02992e1f3e66cf87
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-12-17 15:17:35 +0900

    不要な部分削除

---
+++ lib/utils.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle textStyle(double size, [Color? color, FontWeight? fw]) {
+  return GoogleFonts.montserrat(color: color, fontSize: size, fontWeight: fw);
+}
