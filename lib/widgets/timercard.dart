8a48031aa35f02c95884c89509cabd723e8451d3
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-12-18 10:00:58 +0900

    時間リストのカード追加

--- lib/widgets/timercard.dart
+++ lib/widgets/timercard.dart
@@ -1,11 +1,14 @@
 import "package:flutter/material.dart";
 import 'package:pomodoro_timer/utils.dart';
+import 'package:pomodoro_timer/widgets/timerservice.dart';
+import 'package:provider/provider.dart';
 
 class TimerCard extends StatelessWidget {
   const TimerCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<TimerService>(context);
     return Column(
       children: [
         Text(
@@ -38,6 +41,46 @@ class TimerCard extends StatelessWidget {
                     offset: const Offset(0, 2),
                   )
                 ],
+              ),
+              child: Center(
+                child: Text(
+                  (provider.currentDuration ~/ 60).toString(),
+                  style: textStyle(70, Colors.redAccent),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              ":",
+              style: textStyle(60, Colors.red[200], FontWeight.bold),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 3.2,
+              height: 170,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(
+                  15,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 4,
+                    blurRadius: 4,
+                    offset: const Offset(0, 2),
+                  )
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  "00",
+                  style: textStyle(70, Colors.redAccent),
+                ),
               ),
             ),
           ],
