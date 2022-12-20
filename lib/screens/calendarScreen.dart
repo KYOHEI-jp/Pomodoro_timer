4f457d72878fdb31c57cc2ba21d89397c2e5065b
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-12-20 18:39:47 +0900

    カレンダー画面

---
+++ lib/screens/calendarScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// カレンダー画面
+class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
